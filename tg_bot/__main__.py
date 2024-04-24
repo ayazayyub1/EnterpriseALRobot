96291f7879f0a0a91f162fa9b2740ff6c29819fe
ayazayyub1 <167668295+ayazayyub1@users.noreply.github.com>
2024-04-24 10:05:06 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -714,7 +714,7 @@ def main():
     # dispatcher.add_error_handler(error_handler)
 
     if WEBHOOK:
-        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, allowed_updates=Update.ALL_TYPES, drop_pending_updates=KInit.DROP_UPDATES, webhook_url=URL+TOKEN)
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, allowed_updates=Update.ALL_TYPES, drop_pending_updates=KInit.DROP_UPDATES, wekhook_url= URL)
         log.info("successfully runned")
         log.info(f"Kigyo started, Using long polling. | BOT: [@{dispatcher.bot.username}]")
         KigyoINIT.bot_id = dispatcher.bot.id
