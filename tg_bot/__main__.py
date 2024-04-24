cf200547ff91f0260be153347999c0fcbe3c4a65
ayazayyub1 <167668295+ayazayyub1@users.noreply.github.com>
2024-04-24 08:07:19 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -4,6 +4,7 @@ Dank-del
 2020-12-29
 """
 
+import ssl
 import importlib
 import re
 import threading
@@ -706,7 +707,8 @@ def migrate_chats(update: Update, context: CallbackContext):
     log.info("Successfully migrated!")
     raise DispatcherHandlerStop
 
-cert = "./localhost.pem"
+pem_data = ssl.get_server_certificate((hostname, port))
+cert = x509.load_pem_x509_certificate(pem_data, default_backend())
 
 def main():
     dispatcher.add_error_handler(error_callback)
